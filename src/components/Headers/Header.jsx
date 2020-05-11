92b3169350c1ee52da0c8d2529783d06671c2683
Jose Miranda <jose.miranda@encodely.dev>
2020-05-11 09:02:11 -0600

    Nuevo

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -18,26 +18,12 @@ class Header extends React.Component {
   render() {
     return (
       <>
-        <div className="header bg-gradient-info pb-4 pt-5 pt-md-8">
+        <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
           <Container>
             <Row>
               <Col lg="6" xl="3">
-                <Button
-                  color="warning"
-                  onClick={() =>
-                    this.props.addStateCardAction(
-                      "Performance",
-                      "49,65%",
-                      "fas fa-percent",
-                      "bg-info text-white rounded-circle shadow",
-                      "fas fa-arrow-up",
-                      "text-success",
-                      "12%",
-                      "Since last month"
-                    )
-                  }
-                >
-                  Agregar nuevos usuarios
+                <Button color="success" onClick={() => {}}>
+                  <i className="fa fa-user-plus"> </i> Agregar usuarios
                 </Button>
               </Col>
             </Row>
